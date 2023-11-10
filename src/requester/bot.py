d6ea958d7d89dbee29862b05087a0fdc767f927d
NUCO <93378833+sitting-cat@users.noreply.github.com>
2023-11-10 19:06:28 +0900

    実装をシンプルに

--- src/requester/bot.py
+++ src/requester/bot.py
@@ -5,11 +5,13 @@ import discord
 import logging
 from decouple import config
 from requests import post
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -27,35 +29,33 @@ async def on_ready():
     for l in logo:
         logger.info(l)
 
+
 @client.event
 async def on_message(message):
-    logger = logging.getLogger(__name__)
-    if message.author == client.user:
-        return
+    if message.author != client.user \
+            and isinstance(message.channel, discord.TextChannel) \
+            and message.channel.id == int(config("REQ_WATCH_CHANNEL")):
+        video = getNicoVideoFromString(message.content)
+        if isinstance(video, QuotableVideo):
+            postRequest(video)
+            replyMessage(message, messages[SUCCESS])
+        if isinstance(video, UnquotableVideo):
+            replyMessage(message, messages[UNQUOTABLE])
 
-    logger.debug(f"Received message : {message.content}")
-    matched = re.search(r"[a-z][a-z][0-9]+",message.content)
-    if matched:
-        videoId = matched.group()
-        logger.info(f"Video adding to DB ... : {videoId}")
-        postRequest(videoId)
-        await message.channel.send(f'Video added ! : {videoId}')
 
-def postRequest(item: str):
-    url = config("REQBOT_DB_URL",default=None)
-    key = config("REQBOT_DB_KEY", default=None)
-    if None in (url,key):
-        raise Exception(f"E00 環境変数エラー {url} {key}")
-    if not re.match("^[a-z][a-z][0-9]+$", item):
-        logging.getLogger(__name__).error("E01 予期せぬリクエスト {0}".format(item))
-        return
-    payload = {"videoId": item}
-    headers = {'x-apikey': str(key), 'cache-control': "no-cache"}
-    resp = post(str(url), json=payload, headers=headers)
+def postRequest(item: QuotableVideo):
+    headers = {
+        'x-apikey': config("REQBOT_DB_KEY", cast=str),
+        'cache-control': "no-cache"
+    }
+    resp = post(
+        # NOTE - Url MUST be str.
+        url=config("REQBOT_DB_URL", cast=str),  # type: ignore
+        json={"videoId": item}, headers=headers
+    )
     resp.raise_for_status()
 
+
 def startDiscordBot():
-    DISCORD_TOKEN = config("REQBOT_DISCORD_TOKEN",default=None)
-    if DISCORD_TOKEN is None:
-        raise Exception(f"E00 未設定の環境変数 {DISCORD_TOKEN}")
+    DISCORD_TOKEN = config("REQBOT_DISCORD_TOKEN", default=None)
     client.run(str(DISCORD_TOKEN))
